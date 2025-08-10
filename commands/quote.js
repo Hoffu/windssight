dedbd4a2f8af382310b6c31b49c883c7a1022061
Kirill Petrov <hoffusha@gmail.com>
2025-08-10 12:40:09 +0300

    feat: new command "Quote"

---
+++ commands/quote.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+
+const createEmbed = (quote, author, date) => {
+    return new EmbedBuilder()
+        .setDescription(`"${quote}"`)
+	    .setFooter({ text: `(c) ${author}` })
+        .setTimestamp(date);
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('quote')
+		.setDescription('Golden Quotes Foundation'),
+	async execute(interaction) {
+        const channel = client.channels.fetch("424535278816854017");
+        const rand = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
+        let messages = [];
+        let i = rand(1, 100);
+
+        let message = await channel.messages
+            .fetch({ limit: 1 })
+            .then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
+
+        while (message && i > 0) {
+            i--;
+            await channel.messages
+            .fetch({ limit: 100, before: message.id })
+            .then(messagePage => {
+                messagePage.forEach(msg => messages.push(msg));
+                message = 0 < messagePage.size ? messagePage.at(messagePage.size - 1) : null;
+            });
+        }
+
+        const selectedMessage = messages[rand(0, messages.length)];
+        messages = null;
+        message = null;
+
+        const embed = createEmbed(
+            selectedMessage.content,
+            selectedMessage.author.tag,
+            selectedMessage.createdTimestamp
+        );
+
+        await interaction.reply({ embeds: [embed] });
+	},
+};
